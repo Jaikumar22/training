94740b5d07be680c24ff850def969dbb59d4d0e5
Jaikumar22 <rpjaikumar@outlook.com>
2022-07-23 11:00:36 +0530

    commit

--- JavaTraining/src/com/enoch/oop/MainClass.java
+++ JavaTraining/src/com/enoch/oop/MainClass.java
@@ -1,31 +1,21 @@
 package com.enoch.oop;
 
 public class MainClass {
-	String Name;
-	int age;
-	String address;
-	int mobile_number;
-	
-	public MainClass(String Name,int age,String address,int mobileNO) {
-		this.Name= Name;
-		this.age = age;
-		this.address= address;
-		this.mobile_number= mobileNO;
+	public static void main(String[] args) {
+		Person jaiPerson = new Person();
+		jaiPerson.name="Jai";
+		jaiPerson.age=15;
+		jaiPerson.address="abc";
+		jaiPerson.mobile=123456;
+		System.out.println(jaiPerson.name + " " + jaiPerson.age + " " + jaiPerson.address + " " + jaiPerson.mobile);{
+		Person arunperson = new Person();
+		arunperson.name= "arun";
+		arunperson.age = 18;
+		arunperson.address="xyz";
+		arunperson.mobile=789123;
+		System.out.println(arunperson.name + " " + arunperson.age + " " + arunperson.address + " " + arunperson.mobile);
 		}
-	public void displayDetails() {
-		System.out.println("JaiPerson Details  : " + Name +"\t"+ age +"\t"+ address +"\t"+ mobile_number);
-		System.out.println("arunPerson Details : " + Name +"\t"+ age +"\t"+ address +"\t"+ mobile_number);
-		
-		
-		
 	}
-
-public static void main(String[] args) {
-	MainClass jaiperson = new MainClass("jai",20,"abc",123456);
-	MainClass arunperson = new MainClass("arun", 15, "xyz",54987);
-	jaiperson.displayDetails();
-	arunperson.displayDetails();
-}
 }
 
 
