33ac6eb85c4b8e7aebfa0ca7d634e3bab0818678
Jaikumar22 <rpjaikumar@outlook.com>
2022-07-25 13:30:26 +0530

    new 2

--- JavaTraining/src/com/enoch/oop/MainClass.java
+++ JavaTraining/src/com/enoch/oop/MainClass.java
@@ -9,22 +9,16 @@ public class MainClass {
 		jaiPerson.address="abc";
 		jaiPerson.mobile=123456;
 		System.out.println(jaiPerson.name + " , " + jaiPerson.age + " , " + jaiPerson.address + " , " + jaiPerson.mobile);
+			Person arunperson = new Person();
+			arunperson.name= "arun";
+			arunperson.age = 18;
+			arunperson.address="xyz";
+			arunperson.mobile=789123;
+			System.out.println(arunperson.name + " , " + arunperson.age + " , " + arunperson.address + " , " + arunperson.mobile);
+				School schoolname = new School();
+				schoolname.nameofSchool="GHK School";
+				System.out.println(schoolname.nameofSchool);
 		
-		{
-		Person arunperson = new Person();
-		arunperson.name= "arun";
-		arunperson.age = 18;
-		arunperson.address="xyz";
-		arunperson.mobile=789123;
-		System.out.println(arunperson.name + " , " + arunperson.age + " , " + arunperson.address + " , " + arunperson.mobile);
-		
-		}
-		
-		{
-			School schoolname = new School();
-			schoolname.nameofSchool="GHK School";
-			System.out.println(schoolname.nameofSchool);
-		}
 		
 		
 		}
